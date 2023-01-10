3d966301070be94647504db9191f6f7f1400259f
matan sharon <matca2953@gmail.com>
2023-01-10 16:28:54 +0200

    first commit

---
+++ script.py
@@ -0,0 +1,9 @@
+import os
+from docx2pdf import convert
+source ='C:/ePDM_Elcam/test/FE_Test'
+destintion='C:/Users/matan.s/Desktop/python/add-in_clone'
+files=os.listdir(destintion)
+print(files)
+for file in files:
+    if file.endswith('.docx'):
+        convert(destintion+'/'+file, destintion+'/'+file[:-5]+'.pdf')
