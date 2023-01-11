75025012c0db260468bb2e82f1539333f0a1f99c
matan sharon <matca2953@gmail.com>
2023-01-11 12:17:59 +0200

    finish the stage of converting

--- script.py
+++ script.py
@@ -4,5 +4,9 @@ source ='C:/ePDM_Elcam/test/FE_Test'
 destintion='C:/Users/matan.s/Desktop/python/add-in_clone'
 files=os.listdir(destintion)
 print(files)
-with open('test.text',"w")as file:
-    file.write(str(files))
+with open(destintion+'/test.txt','w') as f:
+    f.write('test')
+for file in files:
+    if file.endswith('.docx'):
+        convert(destintion+'/'+file, destintion+'/'+file[:-5]+'.pdf')
+        print('done')
