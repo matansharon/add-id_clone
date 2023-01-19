bcdbf59ee8b18c3b271f0bba1c5a4a3a44a7275e
matan sharon <matca2953@gmail.com>
2023-01-19 14:33:58 +0200

    get the time modifide

--- prop_read.py
+++ prop_read.py
@@ -2,12 +2,17 @@ import win32com.client
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
+inbox = outlook.GetDefaultFolder(6).Parent.Folders("epdm") # "6" refers to the index of a folder - in this case,
                                     # the inbox. You can change that number to reference
                                     # any other folder
 messages = inbox.Items
-message = messages.GetLast()
-body_content = message.body
-body_content = body_content.split("\n")
-for i in body_content:
-    print(i)
+for i in range(3):
+    m=messages[i]
+    #print the message send date
+    print(m.SentOn) 
+    
+# message = messages.GetLast()
+# body_content = message.body
+# body_content = body_content.split("\n")
+# for i in body_content:
+#     print(i)
